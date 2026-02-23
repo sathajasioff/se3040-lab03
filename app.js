20ca77e030153ed64d468947972a432966279397
satha jasi <151600912+sathajasioff@users.noreply.github.com>
2026-02-23 22:26:15 +0530

    Added async/await example

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Hello SE3040 Lab 03");
+const condition = true;
+
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) {
+    resolve('Success!');
+  } else {
+    reject('Failure!');
+  }
+});
+
+async function myFunction() {
+  try {
+    const result = await myPromise;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+myFunction();
